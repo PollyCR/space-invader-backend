c1303fc51443e37b2b3ebaa7bda99b326866f7d8
oliburt <oliburt@gmail.com>
2019-10-18 11:24:02 +0100

    setup message create action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,4 +3,19 @@ class MessagesController < ApplicationController
         messages = Message.all
         render json: messages
     end
+
+    def create
+        message = Message.new(message_params)
+        if message.save
+            render json: message
+        else
+            render json: {errors: message.errros.full_messages}
+        end
+    end
+
+    private
+
+    def message_params
+        params.require(:message).permit(:user_id, :content, :chatroom_id)
+    end
 end
