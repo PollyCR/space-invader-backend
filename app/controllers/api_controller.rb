fe1c7cf28b6825f72a55c6b527094d2344d869e6
oliburt <oliburt@gmail.com>
2019-10-18 09:10:08 +0100

    added message index action and message serializer

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -14,5 +14,6 @@ class ApiController < ApplicationController
         timeNow = Time.now.strftime("%Y-%m-%d")
         response = JSON.parse(RestClient.get("https://api.nasa.gov/neo/rest/v1/feed?start_date=#{timeNow}&api_key=dDeY37wmDM7xBJg1VaHRAb0dFVHdgDUHQhPzdORm"))
         render json: response
-    end 
+    end
+    
 end
