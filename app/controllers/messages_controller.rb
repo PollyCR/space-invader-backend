60dda4854839085972992f02177d1ab4905a5a13
oliburt <oliburt@gmail.com>
2019-10-19 16:27:35 +0100

    messages channel setup to work with action cable

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,14 @@
 class MessagesController < ApplicationController
-    def index
-        messages = Message.all
-        render json: messages
-    end
 
     def create
         message = Message.new(message_params)
+        chatroom = Chatroom.find(message_params[:chatroom_id])
         if message.save
-            render json: message
-        else
-            render json: {errors: message.errros.full_messages}
+            serialized_data = ActiveModelSerializers::Adapter::Json.new(
+                MessageSerializer.new(message)
+            ).serializable_hash
+            MessagesChannel.broadcast_to chatroom, serialized_data
+            head :ok
         end
     end
 
