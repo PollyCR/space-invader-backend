7ca8a254b804f15067f03573af97150c4a9d31fc
oliburt <oliburt@gmail.com>
2019-10-19 16:26:12 +0100

    stram for messages in chatroom

--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -1,6 +1,7 @@
 class MessagesChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    chatroom = Chatroom.find(params[:chatroom])
+    stream_for chatroom
   end
 
   def unsubscribed
