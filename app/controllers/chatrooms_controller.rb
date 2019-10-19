8d108d23042916e5204300e62aef029a2e87b688
oliburt <oliburt@gmail.com>
2019-10-19 16:27:18 +0100

    chatroom controller setup to work with action cable

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -5,4 +5,21 @@ class ChatroomsController < ApplicationController
         render json: chatrooms
     end
 
+    def create
+        chatroom = Chatroom.new(chatroom_params)
+        if chatroom.save
+          serialized_data = ActiveModelSerializers::Adapter::Json.new(
+            ChatroomSerializer.new(chatroom)
+          ).serializable_hash
+          ActionCable.server.broadcast 'chatrooms_channel', serialized_data
+          head :ok
+        end
+      end
+      
+      private
+      
+      def chatroom_params
+        params.require(:chatroom).permit(:name, :description)
+      end
+
 end
