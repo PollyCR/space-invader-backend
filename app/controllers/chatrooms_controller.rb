7284e4530103ba530ef66fb39ca3c52d861b371a
oliburt <oliburt@gmail.com>
2019-10-23 11:16:08 +0100

    added destroy to chatroom

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -6,20 +6,34 @@ class ChatroomsController < ApplicationController
     end
 
     def create
-        chatroom = Chatroom.new(chatroom_params)
-        if chatroom.save
-          serialized_data = ActiveModelSerializers::Adapter::Json.new(
+      chatroom = Chatroom.new(chatroom_params)
+      if chatroom.save
+        serialized_data = ActiveModelSerializers::Adapter::Json.new(
+          ChatroomSerializer.new(chatroom)
+        ).serializable_hash
+        ActionCable.server.broadcast 'chatrooms_channel', serialized_data
+        head :ok
+      end
+    end
+
+    def destroy
+      chatroom = Chatroom.find(params[:id])
+      chatroom.messages.each{|msg| msg.destroy}
+      if chatroom.destroy
+        serialized_data = ActiveModelSerializers::Adapter::Json.new(
             ChatroomSerializer.new(chatroom)
-          ).serializable_hash
-          ActionCable.server.broadcast 'chatrooms_channel', serialized_data
-          head :ok
-        end
+        ).serializable_hash
+        ActionCable.server.broadcast 'chatrooms_channel', 
+        chatroom: serialized_data,
+        deleted: true
+        head :no_content
       end
+    end
       
-      private
-      
-      def chatroom_params
-        params.require(:chatroom).permit(:name, :description)
-      end
+    private
+    
+    def chatroom_params
+      params.require(:chatroom).permit(:name, :description)
+    end
 
 end
--- app/serializers/chatroom_serializer.rb
+++ app/serializers/chatroom_serializer.rb
@@ -1,4 +1,5 @@
 class ChatroomSerializer < ActiveModel::Serializer
   attributes :id, :description, :name
   has_many :messages
+
 end
